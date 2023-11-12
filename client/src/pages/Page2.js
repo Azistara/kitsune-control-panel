84d8dcb81b0cc42ad03f767e08cf04618e796f2f
Azi <subalt4amail@gmail.com>
2023-11-12 01:13:37 -0800

    Create client server structure

--- client/src/pages/Page2.js
+++ client/src/pages/Page2.js
@@ -1,7 +1,8 @@
 const Page2 = () => {
     return ( 
         <div className="content">
-            <h1>Page 2</h1>
+            <h1>Node JS OS Example Page</h1>
+            
         </div>
      );
 }
